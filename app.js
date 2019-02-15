41738751ec7c646f17ca6287da7a25f05928fb46
Si-Alm <salmgren42@gmail.com>
2019-02-15 12:20:51 -0700

    favicon!

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -18,7 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public/images/favicon.ico')));
 
 
 app.use('/', indexRouter);
